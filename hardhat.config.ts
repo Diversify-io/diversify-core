060912931fa5bab0703fb24600b309172a6cc0a6
vitalragaz <kkrause95@gmail.com>
2021-07-22 14:44:22 +0200

    Init

---
+++ hardhat.config.ts
@@ -0,0 +1,19 @@
+import "@nomiclabs/hardhat-waffle";
+import { task } from "hardhat/config";
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(await account.address);
+  }
+});
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+export default {
+  solidity: "0.7.3",
+};
