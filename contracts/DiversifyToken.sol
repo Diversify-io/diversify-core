612d3f002cd0dd18905bb83425cf750166655e7f
vitalragaz <kkrause95@gmail.com>
2021-07-29 15:49:48 +0200

    Setup Stuff

--- contracts/DiversifyToken.sol
+++ contracts/DiversifyToken.sol
@@ -1,40 +1,48 @@
 // SPDX-License-Identifier: MIT
 // Solidity files have to start with this pragma.
 // It will be used by the Solidity compiler to validate its version.
-pragma solidity ^0.8.4;
-import "hardhat/console.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+pragma solidity ^0.8.0;
+import 'hardhat/console.sol';
+import '@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 
 // This is the main building block for smart contracts.
 contract DiversifyToken is Initializable, ERC20Upgradeable, OwnableUpgradeable {
+    event FoundationWalletTransfered(address indexed previousOwner, address indexed newOwner);
+
     uint256 private constant BURN_STOP_SUPPLY = 100000000 * 10**18;
+    address private _foundationWallet;
 
     function initialize() public initializer {
-        __ERC20_init("Diversify", "DIV");
+        __ERC20_init('Diversify', 'DIV');
         __Ownable_init();
         _mint(msg.sender, 1000000000 * 10**decimals());
     }
 
+    /**
+     * Extend transfer with limit burn and governance wallet
+     */
     function _transfer(
         address from,
         address to,
         uint256 value
     ) internal override {
         (uint256 tTransferAmount, uint256 tBurn) = _getTValues(value);
-        console.log(tBurn);
         _burn(from, tBurn);
         super._transfer(from, to, tTransferAmount);
     }
 
-    function _getTValues(uint256 tAmount)
-        private
-        view
-        returns (uint256, uint256)
-    {
+    /**
+     * Calculates the transfer and burnamount
+     *
+     * `tAmount` transaction amount
+     */
+    function _getTValues(uint256 tAmount) private view returns (uint256, uint256) {
         uint256 tTransferAmount = tAmount;
         uint256 tBurn = 0;
+        uint256 tFound = 0;
+
         if (totalSupply() > BURN_STOP_SUPPLY) {
             tBurn = tAmount / 100;
             if (totalSupply() < BURN_STOP_SUPPLY + tBurn) {
@@ -42,6 +50,20 @@ contract DiversifyToken is Initializable, ERC20Upgradeable, OwnableUpgradeable {
             }
             tTransferAmount = tTransferAmount - tBurn;
         }
+
         return (tTransferAmount, tBurn);
+    }
+
+    /**
+     * @dev Returns the address of the foundation wallet.
+     */
+    function foundationWallet() public view returns (address) {
+        return _foundationWallet;
+    }
+
+    function setFoundationWallet(address newFoundationWallet) public onlyOwner {
+        address oldWallet = newFoundationWallet;
+        _foundationWallet = newFoundationWallet;
+        emit FoundationWalletTransfered(oldWallet, newFoundationWallet);
     }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,10 @@
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-waffle";
-import "@openzeppelin/hardhat-upgrades";
-import "@typechain/hardhat";
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-waffle'
+import '@openzeppelin/hardhat-upgrades'
+import '@typechain/hardhat'
 
-const ALCHEMY_API_KEY = "rXd2olfndKqJLkB5X0SV8-FZkI2diQVo";
-const RINKEBY_PRIVATE_KEY = "d7b50a2df201b3d0365df563564e9e0740261f490ea163671b70e7f2636cee05";
+const ALCHEMY_API_KEY = 'rXd2olfndKqJLkB5X0SV8-FZkI2diQVo'
+const RINKEBY_PRIVATE_KEY = 'd7b50a2df201b3d0365df563564e9e0740261f490ea163671b70e7f2636cee05'
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -13,7 +13,7 @@ export default {
   networks: {
     hardhat: {},
     ganache: {
-      url: "http://127.0.0.1:7545",
+      url: 'http://127.0.0.1:7545',
     },
     rinkeby: {
       url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
@@ -21,7 +21,7 @@ export default {
     },
   },
   solidity: {
-    version: "0.8.4",
+    version: '0.8.4',
     settings: {
       optimizer: {
         enabled: true,
@@ -31,8 +31,8 @@ export default {
         // do not include the metadata hash, since this is machine dependent
         // and we want all generated code to be deterministic
         // https://docs.soliditylang.org/en/v0.7.6/metadata.html
-        bytecodeHash: "none",
+        bytecodeHash: 'none',
       },
     },
   },
-};
+}
