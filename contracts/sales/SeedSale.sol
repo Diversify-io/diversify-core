04f3a32bcf7e9eadba5215b678817a7fded967fa
vitalragaz <kkrause95@gmail.com>
2021-07-30 21:38:32 +0200

    save

--- contracts/sales/SeedSale.sol
+++ contracts/sales/SeedSale.sol
@@ -5,11 +5,12 @@ pragma solidity ^0.8.0;
 import '../utils/RetrieveTokensFeature.sol';
 
 /**
- * Contract to handle timelocked interval based capital releases to an immuatable beneficiary
+ * Contract to handle a seed sale round
  */
-contract SeedSaleVault is RetrieveTokensFeature {
+contract SeedSaleRound is RetrieveTokensFeature {
     using SafeERC20 for IERC20;
 
+    // The State of the seed sale
     enum State {
         Setup,
         Active,
@@ -17,36 +18,50 @@ contract SeedSaleVault is RetrieveTokensFeature {
         Closed
     }
 
-    State private _state;
-
     // ERC20 basic token contract being held
     IERC20 private immutable _token;
-
-    // Invested gweis
-    mapping(address => uint256) private _balances;
-
-    // How many token units a buyer gets per wei
-    uint256 public _rate;
-
-    // Amount of coins to sell in momos
-    uint256 private _goal;
 
     // beneficiary of tokens after the sale ends
     address private immutable _beneficiary;
 
-    // startDate of the lock
-    uint256 private _startDate;
-
     // the duration of the lock / end date
     uint256 private immutable _duration;
+
+    // Invested gweis
+    mapping(address => uint256) private _balances;
+
+    // Tracks the state of the seedsale
+    State private _state;
+
+    // How many token units a buyer gets per wei
+    uint256 private _rate;
+
+    // Amount of coins to sell in momos
+    uint256 private _goal;
+
+    // startDate of the lock
+    uint256 private _startDate;
 
     // Amount of wei raised
     uint256 private _weiRaised;
 
-    event Started(uint256 rate, uint256 goal, uint256 _duration);
+    // Locking period of tokens in days after successful round
+    uint256 private _lockingPeriod;
+
+    event Started(uint256 rate, uint256 goal, uint256 duration, uint256 lockingPeriod);
     event Closed();
     event RefundsEnabled();
     event Refunded(address indexed beneficiary, uint256 weiAmount);
+
+    /*
+     * Event for token purchase logging
+     * @param purchaser who paid for the tokens
+     * @param value weis paid for purchase
+     * @param amount amount of tokens purchased
+     * @param _openingTime Crowdsale opening time
+     * @param _closingTime Crowdsale closing time
+     */
+    event TokenPurchase(address indexed purchaser, uint256 value, uint256 amount);
 
     /**
      * Lorem
@@ -54,26 +69,35 @@ contract SeedSaleVault is RetrieveTokensFeature {
     constructor(
         IERC20 token_,
         address beneficiary_,
-        uint256 duration_
+        uint256 duration_,
+        uint256 lockingPeriod_
     ) {
+        // note: we allow a lockingPeriod
         require(beneficiary_ != address(0));
         require(duration_ > 0);
+        require(address(token_) != address(0));
+
         _token = token_;
         _beneficiary = beneficiary_;
         _duration = duration_ * 1 days;
         _state = State.Setup;
+        _lockingPeriod = lockingPeriod_ * 1 days;
     }
 
     /**
      * @dev starts the vault
      */
     function start(uint256 rate_) public onlyOwner {
+        require(_token.balanceOf(address(this)) > 0);
         require(_state == State.Setup, 'Seed already started');
+        require(_rate > 0);
+
         _rate = rate_;
         _state = State.Active;
         _startDate = block.timestamp;
-        _goal = _token.balanceOf(address(this));
-        emit Started(_rate, _goal, _duration);
+        _goal = _token.balanceOf(address(this)) / _rate;
+
+        emit Started(_rate, _goal, _duration, _lockingPeriod);
     }
 
     /**
@@ -105,26 +129,41 @@ contract SeedSaleVault is RetrieveTokensFeature {
     }
 
     /**
-     * @param investor Investor address
+     * @dev low level token purchase ***DO NOT OVERRIDE***
      */
-    function deposit(address investor) public payable onlyOwner {
+    function buyTokens() public payable {
+        uint256 weiAmount = msg.value;
         require(_state == State.Active);
-        _balances[investor] = _balances[investor] + msg.value;
+        require(block.timestamp < _startDate + _duration, 'End duration reached');
+        require(_msgSender() != address(0));
+        require(weiAmount != 0);
+        require(_weiRaised + weiAmount <= _goal);
+
+        // calculate token amount for event
+        uint256 tokens = _getMomoAmount(weiAmount);
+
+        // update state
+        _weiRaised = _weiRaised + weiAmount;
+
+        _balances[_msgSender()] = _balances[_msgSender()] + msg.value;
+        emit TokenPurchase(_msgSender(), weiAmount, tokens);
     }
 
+    /**
+     * Closes the sale, when enduration reached
+     */
     function close() public onlyOwner {
-        require(
-            _state == State.Active && block.timestamp >= _startDate + _duration,
-            'Not started or timelock end not reached'
-        );
-        emit Closed();
-        retrieveETH(payable(beneficiary()));
-    }
-
-    function enableRefunds() public onlyOwner {
         require(_state == State.Active);
-        _state = State.Refunding;
-        emit RefundsEnabled();
+        require(block.timestamp >= _startDate + _duration, 'End duration not reached');
+
+        if (_weiRaised >= _goal) {
+            _state = State.Closed;
+            emit Closed();
+            retrieveETH(payable(beneficiary()));
+        } else {
+            _state = State.Refunding;
+            emit RefundsEnabled();
+        }
     }
 
     /**
@@ -142,12 +181,11 @@ contract SeedSaleVault is RetrieveTokensFeature {
      * @dev payout the freezed amount of token
      */
     function retrieveFreezedTokens() public {
-        require(
-            _state == State.Closed && block.timestamp >= _startDate + _duration,
-            'Not started or timelock end not reached'
-        );
-        // how to calculate prices '
-        _token.safeTransfer(_msgSender(), _balances[_msgSender()]);
+        require(_state == State.Closed, 'Not closed');
+        require(block.timestamp >= (_startDate + _duration + _lockingPeriod), 'Seed locking period not ended');
+        uint256 momoAmount = _getMomoAmount(_balances[_msgSender()]);
+        _balances[_msgSender()] = 0;
+        _token.safeTransfer(_msgSender(), momoAmount);
     }
 
     /**
@@ -163,11 +201,17 @@ contract SeedSaleVault is RetrieveTokensFeature {
      * @dev retrieve wrongly assigned tokens
      */
     function retrieveETH(address payable to) public override onlyOwner {
-        require(
-            _state == State.Closed && block.timestamp >= _startDate + _duration,
-            'Not started or timelock end not reached'
-        );
+        require(_state == State.Closed, 'Not started or timelock end not reached');
         require(to == beneficiary(), 'You can only transfer tokens to the beneficiary');
         super.retrieveETH(to);
+    }
+
+    /**
+     * @dev Override to extend the way in which ether is converted to tokens.
+     * @param _weiAmount Value in wei to be converted into tokens
+     * @return Number of tokens (momo's) that can be purchased with the specified _weiAmount
+     */
+    function _getMomoAmount(uint256 _weiAmount) internal view returns (uint256) {
+        return _weiAmount * rate();
     }
 }
