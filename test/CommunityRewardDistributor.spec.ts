4a07b1acd542b97c826a9c3118085da1f1098232
Kai Krause <kkrause95@gmail.com>
2021-09-11 23:09:49 +0200

    save

--- test/CommunityRewardDistributor.spec.ts
+++ test/CommunityRewardDistributor.spec.ts
@@ -1,14 +1,11 @@
-import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
-import { UpgradableDiversifyV1 } from '../typechain/UpgradableDiversifyV1'
-import { UpgradableCommunityRewardDistributorV1 } from './../typechain/UpgradableCommunityRewardDistributorV1.d'
-
+/*
 describe('CommunityRewardDistributor', function () {
   let divToken: UpgradableDiversifyV1
   let addr1: SignerWithAddress // owner Wallet
   let addr2: SignerWithAddress
   let addr3: SignerWithAddress
   let communityDistributor: UpgradableCommunityRewardDistributorV1
-/*
+
   this.beforeEach(async () => {
     const [a1, a2, a3] = await ethers.getSigners()
     addr1 = a1
