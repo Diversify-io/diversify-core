b7806a4bdb99d5034511928f5cc08a22c8493d49
Kai Krause <kkrause95@gmail.com>
2021-09-11 17:57:52 +0200

    save

--- test/utils/testHelper.ts
+++ test/utils/testHelper.ts
@@ -3,9 +3,10 @@ import { BigNumber } from 'ethers'
 /*
  * Calculates the amount a sender receives with respecting foundation and burn function
  */
-export const calculateReceivedAmount = (amountSent: BigNumber) =>
+export const calculateReceivedAmount = (amountSent: BigNumber): BigNumber =>
   amountSent.sub(calculateBurnAmount(amountSent)).sub(calculateFoundationAmount(amountSent))
 
-export const calculateFoundationAmount = (amountSent: BigNumber) => amountSent.mul(0.25 * 10 ** 2).div(10 ** 4)
+export const calculateFoundationAmount = (amountSent: BigNumber): BigNumber =>
+  amountSent.mul(0.25 * 10 ** 2).div(10 ** 4)
 
-export const calculateBurnAmount = (amountSent: BigNumber) => amountSent.mul(1.0 * 10 ** 2).div(10 ** 4)
+export const calculateBurnAmount = (amountSent: BigNumber): BigNumber => amountSent.mul(1.0 * 10 ** 2).div(10 ** 4)
