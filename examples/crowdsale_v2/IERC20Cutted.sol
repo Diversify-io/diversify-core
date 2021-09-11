d5c7afcb65308014807056b62f3eb3e98324dd4e
vitalragaz <kkrause95@gmail.com>
2021-09-11 18:00:32 +0200

    removed examples

--- examples/crowdsale_v2/IERC20Cutted.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.0;
-
-interface IERC20Cutted {
-    // Some old tokens are implemented without a retrun parameter (this was prior to the ERC20 standart change)
-    function transfer(address to, uint256 value) external;
-
-    function balanceOf(address who) external view returns (uint256);
-}
