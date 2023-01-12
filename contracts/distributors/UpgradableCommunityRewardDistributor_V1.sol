8c86c7c795775fba3f4e312f5ab9902006e6ed1b
easydomi <wittekdomin@hotmail.de>
2023-01-12 17:02:07 +0100

    typo

--- contracts/distributors/UpgradableCommunityRewardDistributor_V1.sol
+++ contracts/distributors/UpgradableCommunityRewardDistributor_V1.sol
@@ -5,7 +5,7 @@ import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import '../vaults/UpgradableTokenVault.sol';
 
 /**
- * Upgradable Contract that will be used in futuere for the community rewards
+ * Upgradable Contract that will be used in future for the community rewards
  */
 contract UpgradableCommunityRewardDistributor_V1 is UpgradableTokenVault {
 
